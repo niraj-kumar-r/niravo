ca31ff55750451b9f83151e49dd2bb6b11ca1848
Niraj Kumar <nirajk23523@gmail.com>
2023-06-02 15:41:49 +0530

    added react-icons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,35 @@
+"use client";
+// makes this a client component
+// but as we are passing children as props
+// doesn't make the children side components
+
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+import { HiHome } from "react-icons/hi";
+
 interface SidebarProps {
     children: React.ReactNode;
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+    const pathname = usePathname();
+    const routes = useMemo(
+        () => [
+            {
+                icon: HiHome,
+                label: "Home",
+                active: pathname !== "/search",
+                href: "/",
+            },
+            {
+                label: "Search",
+                active: pathname === "/search",
+                href: "/search",
+            },
+        ],
+        [pathname]
+    );
+
     return <div>{children}</div>;
 };
 
