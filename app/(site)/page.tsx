ba712ebd48fd0e6e82e6c82eef02d9649e376c8c
Niraj Kumar <nirajk23523@gmail.com>
2023-06-03 22:46:02 +0530

    Liked Songs Button

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 
 export default function Home() {
     return (
@@ -8,6 +9,13 @@ export default function Home() {
                     <h1 className="text-3xl font-semibold text-white">
                         Welcome back
                     </h1>
+                    <div className="grid grid-cols-1 gap-3 mt-4 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+                        <ListItem
+                            image="/images/liked.png"
+                            name="Liked Songs"
+                            href="liked"
+                        />
+                    </div>
                 </div>
             </Header>
         </div>
