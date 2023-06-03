ff9aaccecbd870569d0955f77bfc7095420f786a
Niraj Kumar <nirajk23523@gmail.com>
2023-06-03 22:53:48 +0530

    basic layout doe

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -18,6 +18,14 @@ export default function Home() {
                     </div>
                 </div>
             </Header>
+            <div className="px-6 mt-2 mb-7">
+                <div className="flex items-center justify-between">
+                    <h1 className="text-2xl font-semibold text-white">
+                        Newest Songs
+                    </h1>
+                </div>
+                <div>List of Songs!</div>
+            </div>
         </div>
     );
 }
