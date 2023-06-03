e07d4dba90afddbfc72de71b26f05af7f4a9aa79
Niraj Kumar <nirajk23523@gmail.com>
2023-06-03 15:47:03 +0530

    Welcome Back text

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,7 +3,13 @@ import Header from "@/components/Header";
 export default function Home() {
     return (
         <div className="w-full h-full overflow-hidden overflow-y-auto rounded-lg bg-neutral-900">
-            <Header>Header</Header>
+            <Header>
+                <div className="mb-2">
+                    <h1 className="text-3xl font-semibold text-white">
+                        Welcome back
+                    </h1>
+                </div>
+            </Header>
         </div>
     );
 }
