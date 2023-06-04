303db756639297307c3b9f1fea4e606fed459acf
Niraj Kumar <nirajk23523@gmail.com>
2023-06-04 15:22:16 +0530

    made UserContext

---
+++ hooks/useUser.tsx
@@ -0,0 +1,20 @@
+import { User } from "@supabase/auth-helpers-nextjs";
+import { createContext } from "react";
+
+import { UserDetails, Subscription } from "../types";
+
+type UserContextType = {
+    accessToken: string | null;
+    user: User | null;
+    userDetails: UserDetails | null;
+    isLoading: boolean;
+    subscription: Subscription | null;
+};
+
+export const UserContext = createContext<UserContextType | undefined>(
+    undefined
+);
+
+export interface Props {
+    [propname: string]: any;
+}
